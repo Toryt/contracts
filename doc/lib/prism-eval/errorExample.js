48438be8050d42d1ec1f746f8bae4e55e9e21c7f
Jan Dockx <jandockx@gmail.com>
2015-11-17 08:59:06 +0100

    labelled output with scripts demonstration / test

--- doc/lib/prism-eval/errorExample.js
+++ doc/lib/prism-eval/errorExample.js
@@ -1,3 +1,4 @@
+Prism.eval.warn("will be shown in a labeled, general output element", "labelled");
 Prism.eval.test.codeExecuted("will throw an error (external code)");
 this.isNotJs();
 Prism.eval.test.codeShouldNotBeExecuted("will not be reached (external code)");
