c58189b630a396afef61b3c6c0e785bcd271adc6
Jan Dockx <jandockx@gmail.com>
2015-11-17 10:26:40 +0100

    Typo

--- doc/lib/prism-eval/errorExample.js
+++ doc/lib/prism-eval/errorExample.js
@@ -1,4 +1,4 @@
-Prism.eval.warn("will be shown in a labeled, general output element", "labelled");
 Prism.eval.test.codeExecuted("will throw an error (external code)");
+Prism.eval.warn("will be shown in a labeled, general output element", "labeled");
 this.isNotJs();
 Prism.eval.test.codeShouldNotBeExecuted("will not be reached (external code)");
