504adaf23d9fe8426649381c15c7933b1ef48450
Jan Dockx <jandockx@gmail.com>
2015-11-29 19:45:23 +0100

    8 & 9 use common fibonacci definition

---
+++ doc/story/fibonacci.js
@@ -0,0 +1,32 @@
+function fibonacci(n) {
+
+  fibonacci.verifyPre(n);
+
+  var result;
+  if (n === 0) {
+    result = 0;
+  }
+  else if (n === 1) {
+    result = 1;
+  }
+  else if (n === 8) {
+    result = -3; // wrong!
+  }
+  else {
+    result = fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  fibonacci.verifyPost(n, result);
+
+  return result;
+}
+
+fibonacci.pre = [
+  function(n) {return Number.isInteger(n);},
+  function(n) {return 0 <= n;}
+];
+fibonacci.post = [
+  function(n, result) {return n !== 0 || result === 0;},
+  function(n, result) {return n !== 1 || result === 1;},
+  function(n, result) {return n < 2 || result === fibonacci(n - 1) + fibonacci(n - 2);}
+];
