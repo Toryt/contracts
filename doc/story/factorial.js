fcf7ca22ba58eeebaa07c2f5674b15338a31e397
Jan Dockx <jandockx@gmail.com>
2015-11-29 20:16:31 +0100

    separate out the factorial contract

--- doc/story/factorial.js
+++ doc/story/factorial.js
@@ -17,12 +17,3 @@ function factorial(n) {
 
   return result;
 }
-
-factorial.pre = [
-  function(n) {return Number.isInteger(n);},
-  function(n) {return 0 <= n;}
-];
-factorial.post = [
-  function(n, result) {return n !== 0 || result === 1;},
-  function(n, result) {return n < 1 || result === n * factorial(n - 1);}
-];
