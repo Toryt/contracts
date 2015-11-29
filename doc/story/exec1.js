6657636a4603344a2bf22d6836df64e73da8ca01
Jan Dockx <jandockx@gmail.com>
2015-11-29 17:13:21 +0100

    rename output of general exec code from "callWith..." to "fibonacciWith..."

--- doc/story/exec1.js
+++ doc/story/exec1.js
@@ -6,19 +6,19 @@
    The parameter must be a positive integer.
    Any errors are logged with Prism.eval. */
 try {
-  Prism.eval.log("\"a\": " + fibonacci("a"), "callWithA");
+  Prism.eval.log("\"a\": " + fibonacci("a"), "fibonacciWithA");
 }
 catch (exc) {
-  Prism.eval.error("\"a\": " + exc, "callWithA");
+  Prism.eval.error("\"a\": " + exc, "fibonacciWithA");
 }
 
 /* Call with all integers from -1 to 9.
    The call with -1 will fail (the parameter must be a positive integer). */
 for (i = -1; i < 10; i++) {
   try {
-    Prism.eval.log(i + ": " + fibonacci(i), "callWithI");
+    Prism.eval.log(i + ": " + fibonacci(i), "fibonacciWithI");
   }
   catch (exc) {
-    Prism.eval.error(i + ": " + exc, "callWithI");
+    Prism.eval.error(i + ": " + exc, "fibonacciWithI");
   }
 }
