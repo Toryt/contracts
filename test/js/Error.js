0921656f95cccc5acd81e6d88fd984ad40b0bee4
Jan Dockx <jandockx@gmail.com>
2016-04-03 20:55:01 +0200

    Moved JS demonstrations / tests to their own "namespace".

---
+++ test/js/Error.js
@@ -0,0 +1,182 @@
+/*
+ Copyright 2015 - 2016 by Jan Dockx
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+(function() {
+  "use strict";
+
+  var expect = require("chai").expect;
+
+  var message = "A message";
+
+  var doLog = true;
+
+  function log() {
+    if (doLog) {
+      console.log.apply(undefined, arguments);
+      console.log();
+    }
+  }
+
+  // describe("js", function() {
+    describe("js/Error", function() {
+      describe("#message", function() {
+        it("has the expected message", function() {
+          var subject = new Error(message);
+          log("message: %s", subject.message);
+          expect(subject.message).to.equal(message);
+        });
+      });
+      describe("#name", function() {
+        it("has the expected name", function() {
+          var subject = new Error(message);
+          log("name: %s", subject.name);
+          expect(subject.name).to.equal("Error");
+        });
+      });
+      describe("#toString()", function() {
+        it("return the name and the message, separated by a colon", function() {
+          var subject = new Error(message);
+          var result = subject.toString();
+          log("toString(): %s", result);
+          expect(result).to.be.a("string");
+          expect(result).to.equal("Error: " + message);
+        });
+      });
+      describe("#fileName", function() {
+        it("has no file name in node", function() {
+          var subject = new Error(message);
+          log("fileName: %s", subject.fileName);
+          //noinspection BadExpressionStatementJS
+          expect(subject.fileName).not.to.be.ok; // not supported in node
+        });
+      });
+      describe("#lineNumber", function() {
+        it("has no line number in node", function() {
+          var subject = new Error(message);
+          log("lineNumber: %s", subject.lineNumber);
+          //noinspection BadExpressionStatementJS
+          expect(subject.lineNumber).not.to.be.ok; // not supported in node
+        });
+      });
+      describe("#columnNumber", function() {
+        it("has no column number in node", function() {
+          var subject = new Error(message);
+          log("columnNumber: %s", subject.columnNumber);
+          //noinspection BadExpressionStatementJS
+          expect(subject.columnNumber).not.to.be.ok; // not supported in node
+        });
+      });
+      describe("#stack", function() {
+        it("has a stack, is a string, that starts with the toString()", function() {
+          var subject = new Error(message);
+          var stack = subject.stack;
+          log("stack: %s", subject.stack);
+          //noinspection BadExpressionStatementJS
+          expect(stack).to.be.ok; // not supported in old IE
+          expect(stack).to.be.a("string");
+          expect(stack.indexOf(subject.toString())).to.equal(0);
+        });
+        it("has a stack, that reports where the error is created", function() {
+          function createAnError() {
+            return new Error(message);
+          }
+
+          function throwAnError() {
+            var anError = createAnError();
+            throw anError;
+          }
+
+          try {
+            throwAnError();
+          }
+          catch(err) {
+            log("err.stack: %s", err.stack);
+            //noinspection BadExpressionStatementJS
+            expect(err.stack).to.be.ok; // not supported in old IE
+            expect(err.stack.indexOf("createAnError")).to.be.at.least(0);
+            // and be on the first line
+            var lines = err.stack.split("\n");
+            lines.forEach(function(l, i) {
+              var found = l.indexOf("createAnError");
+              if (i === 1) {
+                expect(found).to.be.at.least(0);
+              }
+              else {
+                expect(found).to.be.below(0);
+              }
+            });
+          }
+        });
+        it("has a stack, that reports where Error.captureStackTrace is called last", function() {
+          // https://nodejs.org/api/errors.html#errors_error_capturestacktrace_targetobject_constructoropt
+          function createAnError() {
+            return new Error(message);
+          }
+
+          function captureTheStackTrace1(err) {
+            Error.captureStackTrace(err);
+          }
+
+          function throwAnError() {
+            var anError = createAnError();
+            captureTheStackTrace1(anError); // this will be overwritten
+            throw anError;
+          }
+
+          function captureTheStackTrace2(err) {
+            captureTheStackTrace1(err);
+          }
+
+          function captureTheStackTrace3(err) {
+            captureTheStackTrace2(err);
+          }
+
+
+          try {
+            try {
+              throwAnError();
+            }
+            catch(err1) {
+              captureTheStackTrace3(err1);
+              //noinspection ExceptionCaughtLocallyJS
+              throw err1;
+            }
+          }
+          catch (err2) {
+            log("err.stack: %s", err2.stack);
+            //noinspection BadExpressionStatementJS
+            expect(err2.stack).to.be.ok; // not supported in old IE
+            expect(err2.stack.indexOf("createAnError")).to.be.below(0);
+            expect(err2.stack.indexOf("captureTheStackTrace1")).to.be.at.least(0);
+            expect(err2.stack.indexOf("captureTheStackTrace2")).to.be.at.least(0);
+            expect(err2.stack.indexOf("captureTheStackTrace3")).to.be.at.least(0);
+            // and be on the first line
+            var lines = err2.stack.split("\n");
+            lines.forEach(function(l, i) {
+              var found = l.indexOf("captureTheStackTrace");
+              if (1 <= i && i <= 3) {
+                expect(found).to.be.at.least(0);
+              }
+              else {
+                expect(found).to.be.below(0);
+              }
+            });
+          }
+        });
+      });
+    });
+  // });
+})();
