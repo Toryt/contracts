71dc0ae0c7156fca075c50bbb990e5d7356dc319
Jan Dockx <jandockx@gmail.com>
2017-03-16 22:05:19 +0100

    fix documentation

--- src/_private/util.js
+++ src/_private/util.js
@@ -313,15 +313,15 @@
             if (!foundALineOutsideTheLibrary &&
                 line.indexOf(util.contractLibPath) < 0 &&
                 0 <= line.indexOf("/")) {
-              // we found the first line of code that uses util library, if we haven't found such a line earlier,
-              // and we are past the message, and the line does not refer to util library or native code
+              // we found the first line of code not in this library, if we haven't found such a line earlier,
+              // and we are past the message, and the line does not refer to this library or native code
               foundALineOutsideTheLibrary = true;
             }
             if (line.indexOf(util.contractLibPath) < 0 &&
                  (0 <= line.indexOf("/") || foundALineOutsideTheLibrary)) {
-              // copy all the message lines, and the lines not referring to util library that are not referring to
+              // copy all the message lines, and the lines not referring to this library that are not referring to
               // native code, and the lines that are referring to native code once we encountered the first line
-              // of non-native code that refers to code outside util library
+              // of non-native code that refers to code outside this library
               acc.push(line);
             }
             return acc;
