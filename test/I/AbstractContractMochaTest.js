Toryt/contracts

commit 5130a50575dc2867efe95f45b7aa005bcf57dbcb

Demote notes and todo’s. We have a minimum version for node, so this should be ok.

test/I/AbstractContractMochaTest.js
@@ -85,7 +85,7 @@
           var result = AbstractContract.contractFunctionDisplayName(anonymousFunction);
           // in ES6, this function has the name of the variable
           // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
-          // TODO this does not work in older node expect(result).to.be.equal(AbstractContract.displayNamePrefix + "anonymousFunction");
+          expect(result).to.be.equal(AbstractContract.displayNamePrefix + "anonymousFunction");
         });
 
         it("returns the expected display name with an anonymous function with an implementation property that has a display name", function() {
@@ -148,7 +148,7 @@
           var result = AbstractContract.contractFunctionDisplayName(f);
           // in ES6, this function has the name of the variable
           // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
-          // TODO this does not work in older node expect(result).to.be.equal(AbstractContract.displayNamePrefix + "f");
+          expect(result).to.be.equal(AbstractContract.displayNamePrefix + "f");
         });
 
         it("returns the expected display name with an anonymous function as a method", function() {
@@ -159,7 +159,7 @@
           var result = AbstractContract.contractFunctionDisplayName(obj.anonymousFunction);
           // in ES6, this function has the name of the variable
           // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
-          // TODO this does not work in older node expect(result).to.be.equal(Abstract.displayNamePrefix + "anonymousFunction");
+          expect(result).to.be.equal(AbstractContract.displayNamePrefix + "anonymousFunction");
         });
 
         it("returns the expected display name with an named function function as a method", function() {

src/I/ContractError.js
@@ -29,7 +29,7 @@
            PreconditionViolation, PostconditionViolation, ExceptionConditionViolation) {
   "use strict";
 
-  /* NOTE: Custom Error types are notoriously difficult in JavaScript.
+  /* Custom Error types are notoriously difficult in JavaScript.
      See, e.g.,
      - http://stackoverflow.com/questions/1382107/whats-a-good-way-to-extend-error-in-javascript
      - http://pastebin.com/aRpPr5Sd

test/I/ContractFunctionMochaTest.js
@@ -62,7 +62,7 @@
           function(n, result) {return n !== 0 || result === 0;},
           function(n, result) {return n !== 1 || result === 1;},
           function(n, result, fibonacci) {
-            // Note: don't refer to a specific implementation ("fibonacci") in the Contract!
+            // don't refer to a specific implementation ("fibonacci") in the Contract!
             return n < 2 || result === fibonacci(n - 1) + fibonacci(n - 2);
           }
         ],
@@ -98,8 +98,7 @@
           function(n, result) {return util.isInteger(result);},
           function(n, result) {return n !== 0 || result === 1;},
           function(n, result, f) {
-            // Note: don't refer to a specific implementation in the Contract!
-
+            // don't refer to a specific implementation in the Contract!
             return n < 1 || result === n * f(n - 1);
           }
         ],

commit 08720376b65a5038e1079670537ca73621940d4d

Commit 5130a50575dc2867efe95f45b7aa005bcf57dbcb introduced a test error in Firefox 52 and earlier. This will be fixed in FF 53. Documented.
Anonymous functions get the name of the variable they are declared in in ES2015. This is, at 2017-03-26, implemented in node, Chrome and Safari on Mac, but not yet in Firefox 52.
Firefox 53, based on Gecko 53, will ship in April 2017, and will have this functionality: https://developer.mozilla.org/en-US/Firefox/Releases/53

test/I/AbstractContractMochaTest.js
@@ -84,6 +84,9 @@
           var result = AbstractContract.contractFunctionDisplayName(anonymousFunction);
           // in ES6, this function has the name of the variable
           // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+          // This is, at 2017-03-26, implemented in node, Chrome and Safari on Mac, but not yet in Firefox 52.
+          // Firefox 53, based on Gecko 53, will ship in April 2017, and will have this functionality:
+          // https://developer.mozilla.org/en-US/Firefox/Releases/53
           expect(result).to.be.equal(AbstractContract.displayNamePrefix + "anonymousFunction");
         });
 
@@ -147,6 +150,9 @@
           var result = AbstractContract.contractFunctionDisplayName(f);
           // in ES6, this function has the name of the variable
           // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+          // This is, at 2017-03-26, implemented in node, Chrome and Safari on Mac, but not yet in Firefox 52.
+          // Firefox 53, based on Gecko 53, will ship in April 2017, and will have this functionality:
+          // https://developer.mozilla.org/en-US/Firefox/Releases/53
           expect(result).to.be.equal(AbstractContract.displayNamePrefix + "f");
         });
 
@@ -158,6 +164,9 @@
           var result = AbstractContract.contractFunctionDisplayName(obj.anonymousFunction);
           // in ES6, this function has the name of the variable
           // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+          // This is, at 2017-03-26, implemented in node, Chrome and Safari on Mac, but not yet in Firefox 52.
+          // Firefox 53, based on Gecko 53, will ship in April 2017, and will have this functionality:
+          // https://developer.mozilla.org/en-US/Firefox/Releases/53
           expect(result).to.be.equal(AbstractContract.displayNamePrefix + "anonymousFunction");
         });
 