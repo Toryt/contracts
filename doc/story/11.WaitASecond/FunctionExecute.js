9b37250477602d7957887403071fc625d2e2bd77
Jan Dockx <jandockx@gmail.com>
2017-06-04 23:05:42 +0200

    Remove the doc folder from master
    This folder contains the reasoning to get to the early steps of the design, in literate programming style.
    It is of no use in the current state, and would confuse new users, so we are better of without it now.
    
    Saved in branch history/evolution_doc.

--- doc/story/11.WaitASecond/FunctionExecute.js
+++
@@ -1,7 +0,0 @@
-Function.constructor.prototype.execute = function() {
-  this.verifyPre.apply(this, arguments);
-  var result = this.apply(null, arguments);
-  Array.prototype.push.call(arguments, result);
-  this.verifyPost.apply(this, arguments);
-  return result;
-};
--- doc/story/factorialContract.js
+++
@@ -1,8 +0,0 @@
-factorial.pre = [
-  function(n) {return Number.isInteger(n);},
-  function(n) {return 0 <= n;}
-];
-factorial.post = [
-  function(n, result) {return n !== 0 || result === 1;},
-  function(n, result) {return n < 1 || result === n * factorial(n - 1);}
-];
