faa1f92bd9f8b04004259532f5c0bca345669e24
Jan Dockx <jandockx@gmail.com>
2018-05-01 13:40:24 +0200

    evolve testUtil to es and standard

--- test/_util/testUtil.js
+++ test/_util/testUtil.js
@@ -14,228 +14,210 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-(function(factory) {
-  "use strict";
+'use strict'
 
-  var dependencies = ["./expect", "𝕋合同/_private/util"];
+const util = require('../../src/_private/util')
+const expect = require('chai').expect
 
-  if (typeof define === "function" && define.amd) {
-    define(dependencies, factory);
+// noinspection FunctionNamingConventionJS
+function x () {
+  if (arguments.length <= 0) {
+    return []
   }
-  else if (typeof exports === "object") {
-    module.exports =
-      factory.apply(undefined, dependencies.map(function(d) {return require(d.replace("𝕋合同", "../../src"));}));
+  return Array.prototype.reduce.call(
+    arguments,
+    (acc, arrayI) => {
+      const ret = []
+      acc.forEach(elementSoFar => { arrayI.forEach(elementOfI => { ret.push(elementSoFar.concat([elementOfI])) }) })
+      return ret
+    },
+    [[]]
+  )
+}
+
+function expectOwnFrozenProperty (subject, propertyName) {
+  expect(subject).to.have.ownPropertyDescriptor(propertyName)
+  expect(subject).ownPropertyDescriptor(propertyName).to.have.property('enumerable', true)
+  expect(subject).ownPropertyDescriptor(propertyName).to.have.property('configurable', false)
+  expect(subject).ownPropertyDescriptor(propertyName).to.have.property('writable', false)
+  expect(function () {
+    // noinspection MagicNumberJS
+    subject[propertyName] = 42 + ' some outlandish other value'
+  }).to.throw(TypeError)
+}
+
+// noinspection FunctionNamingConventionJS
+function prototypeThatHasOwnPropertyDescriptor (subject, propertyName) {
+  if (!subject) {
+    return subject
   }
-}(function(expect, util) {
-  "use strict";
-
-  //noinspection FunctionNamingConventionJS
-  function x() {
-    if (arguments.length <= 0) {
-      return [];
-    }
-    return Array.prototype.reduce.call(
-      arguments,
-      function(acc, arrayI) {
-        var ret = [];
-        acc.forEach(function(elementSoFar) {
-          arrayI.forEach(function(elementOfI) {
-            ret.push(elementSoFar.concat([elementOfI]));
-          });
-        });
-        return ret;
-      },
-      [[]]
-    );
+  if (Object.getOwnPropertyDescriptor(subject, propertyName)) {
+    return subject
   }
+  return prototypeThatHasOwnPropertyDescriptor(Object.getPrototypeOf(subject), propertyName)
+}
 
-  function expectOwnFrozenProperty(subject, propertyName) {
-    //noinspection JSUnresolvedFunction
-    expect(subject).to.have.ownPropertyDescriptor(propertyName);
-    //noinspection JSUnresolvedFunction
-    expect(subject).ownPropertyDescriptor(propertyName).to.have.property("enumerable", true);
-    //noinspection JSUnresolvedFunction
-    expect(subject).ownPropertyDescriptor(propertyName).to.have.property("configurable", false);
-    //noinspection JSUnresolvedFunction
-    expect(subject).ownPropertyDescriptor(propertyName).to.have.property("writable", false);
-    expect(function() {
-      //noinspection MagicNumberJS
-      subject[propertyName] = 42 + " some outlandish other value";
-    }).to.throw(TypeError);
+// noinspection FunctionNamingConventionJS
+function expectDerivedPropertyOnAPrototype (subject, propertyName, configurable) {
+  const prototype = prototypeThatHasOwnPropertyDescriptor(subject, propertyName)
+  expect(prototype).to.have.ownPropertyDescriptor(propertyName)
+  expect(prototype).ownPropertyDescriptor(propertyName).to.have.property('enumerable', true)
+  expect(prototype).ownPropertyDescriptor(propertyName).to.have.property('configurable', configurable)
+  expect(prototype).ownPropertyDescriptor(propertyName).not.to.have.property('writable')
+  expect(prototype).ownPropertyDescriptor(propertyName).to.have.property('get').that.is.a('function')
+  // eslint-disable-next-line
+  expect(prototype).ownPropertyDescriptor(propertyName).to.have.property('set').that.is.not.ok
+}
+
+// noinspection FunctionNamingConventionJS
+function expectConfigurableDerivedPropertyOnAPrototype (subject, propertyName) {
+  expectDerivedPropertyOnAPrototype(subject, propertyName, true)
+}
+
+// noinspection FunctionNamingConventionJS
+function expectFrozenDerivedPropertyOnAPrototype (subject, propertyName) {
+  expectDerivedPropertyOnAPrototype(subject, propertyName, false)
+}
+
+function expectFrozenPropertyOnAPrototype (subject, propertyName) {
+  const prototype = prototypeThatHasOwnPropertyDescriptor(subject, propertyName)
+  expectOwnFrozenProperty(prototype, propertyName)
+}
+
+// noinspection FunctionNamingConventionJS
+function expectFrozenReadOnlyArrayPropertyWithPrivateBackingField (subject, propName, privatePropName) {
+  expect(subject).to.have.ownProperty(privatePropName) // array not shared
+  expect(subject).to.have.property(privatePropName).that.is.an('array')
+  this.expectOwnFrozenProperty(subject, privatePropName)
+  expect(subject).to.have.property(propName).that.is.an('array')
+  this.expectFrozenDerivedPropertyOnAPrototype(subject, propName)
+  expect(function () {
+    // noinspection MagicNumberJS
+    subject[propName] = 42 + ' some outlandish other value'
+  }).to.throw(TypeError)
+}
+
+function expectToBeArrayOfFunctions (a) {
+  expect(a).to.be.an('array')
+  a.forEach(element => { expect(element).to.be.a('function') })
+}
+
+const doLog = false
+
+// noinspection FunctionNamingConventionJS
+function log () {
+  if (doLog) {
+    console.log.apply(undefined, arguments)
+    console.log()
   }
+}
 
-  //noinspection FunctionNamingConventionJS
-  function prototypeThatHasOwnPropertyDescriptor(subject, propertyName) {
-    if (!subject) {
-      return subject;
-    }
-    if (Object.getOwnPropertyDescriptor(subject, propertyName)) {
-      return subject;
-    }
-    return prototypeThatHasOwnPropertyDescriptor(Object.getPrototypeOf(subject), propertyName);
+function showStack (exc) {
+  log('Exception stack%s---------------%s%s', util.eol, util.eol, exc.stack)
+}
+
+function regExpEscape (s) {
+  // http://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript
+  return s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
+}
+
+function propertyIsWritable (object, propertyName) {
+  const prototype = prototypeThatHasOwnPropertyDescriptor(object, propertyName)
+  const pd = prototype && Object.getOwnPropertyDescriptor(prototype, propertyName)
+  return !pd || pd.writable
+}
+
+function anyCasesGenerators (discriminator) {
+  // noinspection JSPrimitiveTypeWrapperUsage,MagicNumberJS
+  return [
+    () => new Error('This is a ' + discriminator + ' case'),
+    () => undefined,
+    () => null,
+    () => 1,
+    () => 0,
+    () => 'a string that is used as a ' + discriminator,
+    () => '',
+    () => true,
+    () => false,
+    () => new Date(),
+    () => /foo/,
+    () => function () { return 'this simulates a ' + discriminator },
+    // eslint-disable-next-line
+    () => new Number(42),
+    // eslint-disable-next-line
+    () => new Boolean(false),
+    // eslint-disable-next-line
+    () => new String(discriminator + ' string'),
+    () => arguments,
+    () => ({}),
+    () => ({a: 1, b: 'b', c: {}, d: {d1: undefined, d2: 'd2', d3: {d31: 31}}})
+  ]
+}
+
+// http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
+function environment () {
+  // eslint-disable-next-line
+  if ((new Function('try {return this === global;}catch(e){return false;}'))()) {
+    return 'node'
   }
-
-  //noinspection FunctionNamingConventionJS
-  function expectDerivedPropertyOnAPrototype(subject, propertyName, configurable) {
-    var prototype = prototypeThatHasOwnPropertyDescriptor(subject, propertyName);
-    //noinspection JSUnresolvedFunction
-    expect(prototype).to.have.ownPropertyDescriptor(propertyName);
-    //noinspection JSUnresolvedFunction
-    expect(prototype).ownPropertyDescriptor(propertyName).to.have.property("enumerable", true);
-    //noinspection JSUnresolvedFunction
-    expect(prototype).ownPropertyDescriptor(propertyName).to.have.property("configurable", configurable);
-    //noinspection JSUnresolvedFunction
-    expect(prototype).ownPropertyDescriptor(propertyName).not.to.have.property("writable");
-    //noinspection JSUnresolvedFunction
-    expect(prototype).ownPropertyDescriptor(propertyName).to.have.property("get").that.is.a("function");
-    //noinspection JSUnresolvedFunction,BadExpressionStatementJS,JSHint
-    expect(prototype).ownPropertyDescriptor(propertyName).to.have.property("set").that.is.not.ok;
+  // noinspection JSUnresolvedVariable
+  if (
+    // eslint-disable-next-line
+    (!!window.opr && !!opr.addons) ||
+    !!window.opera ||
+    navigator.userAgent.indexOf(' OPR/') >= 0
+  ) {
+    return 'opera'
   }
-
-  //noinspection FunctionNamingConventionJS
-  function expectConfigurableDerivedPropertyOnAPrototype(subject, propertyName) {
-    expectDerivedPropertyOnAPrototype(subject, propertyName, true);
+  // noinspection JSUnresolvedVariable
+  if (typeof InstallTrigger !== 'undefined') {
+    return 'firefox'
   }
-
-  //noinspection FunctionNamingConventionJS
-  function expectFrozenDerivedPropertyOnAPrototype(subject, propertyName) {
-    expectDerivedPropertyOnAPrototype(subject, propertyName, false);
+  // noinspection JSUnresolvedVariable
+  if (/constructor/i.test(window.HTMLElement) ||
+      (function (p) {
+        return p.toString() === '[object SafariRemoteNotification]'
+      })(
+        // eslint-disable-next-line
+        !window['safari'] || safari.pushNotification
+      )) {
+    return 'safari'
   }
-
-  function expectFrozenPropertyOnAPrototype(subject, propertyName) {
-    var prototype = prototypeThatHasOwnPropertyDescriptor(subject, propertyName);
-    expectOwnFrozenProperty(prototype, propertyName);
+  // noinspection PointlessBooleanExpressionJS,JSUnresolvedVariable
+  if (/* @cc_on!@ */false || !!document.documentMode) {
+    return 'ie'
   }
-
-  //noinspection FunctionNamingConventionJS
-  function expectFrozenReadOnlyArrayPropertyWithPrivateBackingField(subject, propName, privatePropName) {
-    expect(subject).to.have.ownProperty(privatePropName); // array not shared
-    expect(subject).to.have.property(privatePropName).that.is.an("array");
-    this.expectOwnFrozenProperty(subject, privatePropName); // jshint ignore:line
-    expect(subject).to.have.property(propName).that.is.an("array");
-    this.expectFrozenDerivedPropertyOnAPrototype(subject, propName); // jshint ignore:line
-    expect(function() {
-      //noinspection MagicNumberJS
-      subject[propName] = 42 + " some outlandish other value";
-    }).to.throw(TypeError);
+  // noinspection JSUnresolvedVariable
+  if (window.StyleMedia) {
+    return 'edge'
   }
-
-  function expectToBeArrayOfFunctions(a) {
-    expect(a).to.be.an("array");
-    a.forEach(function(element) {
-      expect(element).to.be.a("function");
-    });
+  // noinspection JSUnresolvedVariable
+  if (!!window.chrome && !!window.chrome.webstore) {
+    return 'chrome'
   }
-
-  var doLog = false;
-
-  //noinspection FunctionNamingConventionJS
-  function log() {
-    if (doLog) {
-      console.log.apply(undefined, arguments);
-      console.log();
-    }
+  // noinspection JSUnresolvedVariable
+  if (window.CSS) {
+    return 'blink'
   }
-
-  function showStack(exc) {
-    log("Exception stack%s---------------%s%s", util.eol, util.eol, exc.stack);
+  // eslint-disable-next-line
+  if ((new Function('try {return this === window;}catch(e){ return false;}'))()) {
+    return 'browser'
   }
+  return undefined
+}
 
-  function regExpEscape(s) {
-    //http://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript
-    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
-  }
-
-  function propertyIsWritable(object, propertyName) {
-    var prototype = prototypeThatHasOwnPropertyDescriptor(object, propertyName);
-    var pd = prototype && Object.getOwnPropertyDescriptor(prototype, propertyName);
-    return !pd || pd.writable;
-  }
-
-  function anyCasesGenerators(discriminator) {
-    return [
-      function() {return new Error("This is a " + discriminator + " case");},
-      function() {return undefined;},
-      function() {return null;},
-      function() {return 1;},
-      function() {return 0;},
-      function() {return "a string that is used as a " + discriminator;},
-      function() {return "";},
-      function() {return true;},
-      function() {return false;},
-      function() {return new Date();},
-      function() {return /foo/;},
-      function() {return function() {return "this simulates a " + discriminator;};},
-      function() {
-        //noinspection JSPrimitiveTypeWrapperUsage,JSHint,MagicNumberJS
-        return new Number(42);
-      },
-      function() {
-        //noinspection JSPrimitiveTypeWrapperUsage,JSHint
-        return new Boolean(false);
-      },
-      function() {
-        //noinspection JSPrimitiveTypeWrapperUsage,JSHint
-        return new String(discriminator + " string");
-      },
-      function() {return arguments;},
-      function() {return {};},
-      function() {//noinspection MagicNumberJS
-        return {a: 1, b: "b", c: {}, d: {d1: undefined, d2: "d2", d3: {d31: 31}}};
-      }
-    ];
-  }
-
-  // http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
-  function environment() { // jshint ignore:line
-    if ((new Function("try {return this === global;}catch(e){return false;}"))()) { // jshint ignore:line
-      return "node";
-    }
-    if ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(" OPR/") >= 0) { // jshint ignore:line
-      return "opera";
-    }
-    if (typeof InstallTrigger !== "undefined") {
-      return "firefox";
-    }
-    if (/constructor/i.test(window.HTMLElement)
-        || (function(p) {
-              return p.toString() === "[object SafariRemoteNotification]";
-             })(!window["safari"] || safari.pushNotification)) { // jshint ignore:line
-      return "safari";
-    }
-    //noinspection PointlessBooleanExpressionJS
-    if (/*@cc_on!@*/false || !!document.documentMode) {
-      return "ie";
-    }
-    if (!!window.StyleMedia) {
-      return "edge";
-    }
-    if (!!window.chrome && !!window.chrome.webstore) {
-      return "chrome";
-    }
-    if (!!window.CSS) {
-      return "blink";
-    }
-    if ((new Function("try {return this === window;}catch(e){ return false;}"))()) { // jshint ignore:line
-      return "browser";
-    }
-    return undefined;
-  }
-
-  return {
-    x: x,
-    expectOwnFrozenProperty: expectOwnFrozenProperty,
-    expectConfigurableDerivedPropertyOnAPrototype: expectConfigurableDerivedPropertyOnAPrototype,
-    expectFrozenDerivedPropertyOnAPrototype: expectFrozenDerivedPropertyOnAPrototype,
-    expectFrozenReadOnlyArrayPropertyWithPrivateBackingField: expectFrozenReadOnlyArrayPropertyWithPrivateBackingField,
-    expectFrozenPropertyOnAPrototype: expectFrozenPropertyOnAPrototype,
-    expectToBeArrayOfFunctions: expectToBeArrayOfFunctions,
-    log: log,
-    showStack: showStack,
-    regExpEscape: regExpEscape,
-    propertyIsWritable: propertyIsWritable,
-    anyCasesGenerators: anyCasesGenerators,
-    environment: environment()
-  };
-
-}));
+module.exports = {
+  x: x,
+  expectOwnFrozenProperty: expectOwnFrozenProperty,
+  expectConfigurableDerivedPropertyOnAPrototype: expectConfigurableDerivedPropertyOnAPrototype,
+  expectFrozenDerivedPropertyOnAPrototype: expectFrozenDerivedPropertyOnAPrototype,
+  expectFrozenReadOnlyArrayPropertyWithPrivateBackingField: expectFrozenReadOnlyArrayPropertyWithPrivateBackingField,
+  expectFrozenPropertyOnAPrototype: expectFrozenPropertyOnAPrototype,
+  expectToBeArrayOfFunctions: expectToBeArrayOfFunctions,
+  log: log,
+  showStack: showStack,
+  regExpEscape: regExpEscape,
+  propertyIsWritable: propertyIsWritable,
+  anyCasesGenerators: anyCasesGenerators,
+  environment: environment()
+}
