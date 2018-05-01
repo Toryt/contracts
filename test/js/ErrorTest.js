32a1bafe4f8973ee32548aae5a6571d4607c6c6e
Jan Dockx <jandockx@gmail.com>
2018-05-01 17:15:39 +0200

    js/Error: remove AMD, standardify, es-ify, use arrow functions where appropriate

--- test/js/ErrorTest.js
+++ test/js/ErrorTest.js
@@ -14,181 +14,162 @@
  limitations under the License.
  */
 
-(function(factory) {
-  "use strict";
+/* eslint-env mocha */
 
-  var dependencies = ["../_util/describe", "../_util/it", "../_util/expect", "../_util/testUtil"];
+'use strict'
 
-  if (typeof define === "function" && define.amd) {
-    define(dependencies, factory);
-  }
-  else if (typeof exports === "object") {
-    module.exports = factory.apply(undefined, dependencies.map(function(d) {return require(d);}));
-  }
-}(function(describe, it, expect, testUtil) {
-  "use strict";
+const testUtil = require('../_util/testUtil')
+const must = require('must')
 
-  var message = "A message";
-  var env = testUtil.environment;
-  var isFF = (env === "firefox");
-  var isFFOrSafari = isFF || (env === "safari");
+const message = 'A message'
+const env = testUtil.environment
+const isFF = (env === 'firefox')
+const isFFOrSafari = isFF || (env === 'safari')
 
-  // describe("js", function() {
-    describe("js/Error", function() {
-      describe("#message", function() {
-        it("has the expected message", function() {
-          var subject = new Error(message);
-          testUtil.log("message: %s", subject.message);
-          expect(subject.message).to.equal(message);
-        });
-      });
-      describe("#name", function() {
-        it("has the expected name", function() {
-          var subject = new Error(message);
-          testUtil.log("name: %s", subject.name);
-          expect(subject.name).to.equal("Error");
-        });
-      });
-      describe("#toString()", function() {
-        it("return the name and the message, separated by a colon", function() {
-          var subject = new Error(message);
-          var result = subject.toString();
-          testUtil.log("toString(): %s", result);
-          expect(result).to.be.a("string");
-          expect(result).to.equal("Error: " + message);
-        });
-      });
-      describe("#fileName", function() {
-        it("has " + (isFF ? "a" : "no") +" file name in " + env, function() {
-          var subject = new Error(message);
-          testUtil.log("fileName: %s", subject.fileName);
-          //noinspection BadExpressionStatementJS,JSHint
-          expect(subject.fileName)[isFF ? "to" : "not"].to.be.ok; // not supported in node
-        });
-      });
-      describe("#lineNumber", function() {
-        it("has " + (isFF ? "a" : "no") + " line number in " + env, function() {
-          var subject = new Error(message);
-          testUtil.log("lineNumber: %s", subject.lineNumber);
-          //noinspection BadExpressionStatementJS,JSHint
-          expect(subject.lineNumber)[isFF ? "to" : "not"].to.be.ok; // not supported in node
-        });
-      });
-      describe("#columnNumber", function() {
-        it("has " + (isFF ? "a" : "no") + " column number in " + env, function() {
-          var subject = new Error(message);
-          testUtil.log("columnNumber: %s", subject.columnNumber);
-          //noinspection BadExpressionStatementJS,JSHint
-          expect(subject.columnNumber)[isFF ? "to" : "not"].to.be.ok; // not supported in node
-        });
-      });
-      describe("#stack", function() {
-        it("has a stack, that is a string, that " + (isFFOrSafari ? "does not start" : "starts")
-           + " with the toString() on " + env,
-          function() {
-            var subject = new Error(message);
-            var stack = subject.stack;
-            testUtil.log("stack: %s", subject.stack);
-            //noinspection BadExpressionStatementJS,JSHint
-            expect(stack).to.be.ok; // not supported in old IE
-            expect(stack).to.be.a("string");
-            if (isFFOrSafari) {
-              expect(stack).not.to.contain(subject.toString());
-            }
-            else {
-              expect(stack.indexOf(subject.toString())).to.equal(0);
-            }
-          }
-        );
-        it("has a stack, that reports where the error is created", function() {
-          function createAnError() {
-            return new Error(message);
-          }
+describe('js/Error', function () {
+  describe('#message', function () {
+    it('has the expected message', function () {
+      const subject = new Error(message)
+      testUtil.log('message: %s', subject.message)
+      subject.message.must.equal(message)
+    })
+  })
+  describe('#name', function () {
+    it('has the expected name', function () {
+      const subject = new Error(message)
+      testUtil.log('name: %s', subject.name)
+      subject.name.must.equal('Error')
+    })
+  })
+  describe('#toString()', function () {
+    it('return the name and the message, separated by a colon', function () {
+      const subject = new Error(message)
+      const result = subject.toString()
+      testUtil.log('toString(): %s', result)
+      result.must.be.a.string()
+      result.must.equal('Error: ' + message)
+    })
+  })
+  describe('#fileName', function () {
+    it('has ' + (isFF ? 'a' : 'no') + ' file name in ' + env, function () {
+      const subject = new Error(message)
+      testUtil.log('fileName: %s', subject.fileName)
+      must(subject.fileName).be[isFF ? 'truthy' : 'falsy']() // not supported in node
+    })
+  })
+  describe('#lineNumber', function () {
+    it('has ' + (isFF ? 'a' : 'no') + ' line number in ' + env, function () {
+      const subject = new Error(message)
+      testUtil.log('lineNumber: %s', subject.lineNumber)
+      must(subject.lineNumber).be[isFF ? 'truthy' : 'falsy']() // not supported in node
+    })
+  })
+  describe('#columnNumber', function () {
+    it('has ' + (isFF ? 'a' : 'no') + ' column number in ' + env, function () {
+      const subject = new Error(message)
+      // noinspection JSUnresolvedVariable
+      testUtil.log('columnNumber: %s', subject.columnNumber)
+      // noinspection JSUnresolvedVariable
+      must(subject.columnNumber).be[isFF ? 'truthy' : 'falsy']() // not supported in node
+    })
+  })
+  describe('#stack', function () {
+    it(
+      'has a stack, that is a string, that ' + (isFFOrSafari ? 'does not start' : 'starts') +
+       ' with the toString() on ' + env,
+      function () {
+        const subject = new Error(message)
+        const stack = subject.stack
+        testUtil.log('stack: %s', subject.stack)
+        // noinspection BadExpressionStatementJS,JSHint
+        stack.must.be.truthy() // not supported in old IE
+        stack.must.be.a.string()
+        if (isFFOrSafari) {
+          stack.must.not.contain(subject.toString())
+        } else {
+          stack.indexOf(subject.toString()).must.equal(0)
+        }
+      }
+    )
+    it('has a stack, that reports where the error is created', function () {
+      function createAnError () {
+        return new Error(message)
+      }
 
-          function throwAnError() {
-            //noinspection UnnecessaryLocalVariableJS
-            var anError = createAnError();
-            throw anError;
-          }
+      function throwAnError () {
+        throw createAnError()
+      }
 
-          try {
-            throwAnError();
-          }
-          catch(err) {
-            testUtil.log("err.stack: %s", err.stack);
-            //noinspection BadExpressionStatementJS,JSHint
-            expect(err).to.have.property("stack").that.is.ok; // not supported in old IE
-            expect(err).to.have.property("stack").that.contains("createAnError");
-            // and be on the first line
-            var lines = err.stack.split("\n");
-            if (lines[0] === err.toString()) {
-              // some environments add the toString of the error on the first line
-              lines.shift();
-            }
-            lines.forEach(function(l, i) {
-              expect(l).to[i === 0 ? "to" : "not"].contain("createAnError");
-            });
-          }
-        });
-        it("has a stack, that reports where Error.captureStackTrace is called last", function() {
-          // https://nodejs.org/api/errors.html#errors_error_capturestacktrace_targetobject_constructoropt
-          function createAnError() {
-            return new Error(message);
-          }
+      try {
+        throwAnError()
+      } catch (err) {
+        testUtil.log('err.stack: %s', err.stack)
+        err.stack.must.be.truthy()
+        err.stack.must.contain('createAnError')
+        // and be on the first line
+        const lines = err.stack.split('\n')
+        if (lines[0] === err.toString()) {
+          // some environments add the toString of the error on the first line
+          lines.shift()
+        }
+        lines.forEach((l, i) => {
+          l.must[i === 0 ? 'to' : 'not'].contain('createAnError')
+        })
+      }
+    })
+    it('has a stack, that reports where Error.captureStackTrace is called last', function () {
+      // https://nodejs.org/api/errors.html#errors_error_capturestacktrace_targetobject_constructoropt
+      function createAnError () {
+        return new Error(message)
+      }
 
-          //noinspection FunctionNamingConventionJS
-          function captureTheStackTrace1(err) {
-            Error.captureStackTrace(err);
-          }
+      // noinspection FunctionNamingConventionJS
+      function captureTheStackTrace1 (err) {
+        Error.captureStackTrace(err)
+      }
 
-          function throwAnError() {
-            var anError = createAnError();
-            captureTheStackTrace1(anError); // this will be overwritten
-            throw anError;
-          }
+      function throwAnError () {
+        const anError = createAnError()
+        captureTheStackTrace1(anError) // this will be overwritten
+        throw anError
+      }
 
-          //noinspection FunctionNamingConventionJS
-          function captureTheStackTrace2(err) {
-            captureTheStackTrace1(err);
-          }
+      // noinspection FunctionNamingConventionJS
+      function captureTheStackTrace2 (err) {
+        captureTheStackTrace1(err)
+      }
 
-          //noinspection FunctionNamingConventionJS
-          function captureTheStackTrace3(err) {
-            captureTheStackTrace2(err);
-          }
+      // noinspection FunctionNamingConventionJS
+      function captureTheStackTrace3 (err) {
+        captureTheStackTrace2(err)
+      }
 
-
-          try {
-            try {
-              throwAnError();
-            }
-            catch(err1) {
-              captureTheStackTrace3(err1);
-              //noinspection ExceptionCaughtLocallyJS
-              throw err1;
-            }
-          }
-          catch (err2) {
-            testUtil.log("err.stack: %s", err2.stack);
-            //noinspection BadExpressionStatementJS,JSHint
-            expect(err2).to.have.property("stack").that.is.ok; // not supported in old IE
-            expect(err2).to.have.property("stack").that.not.contains("createAnError");
-            expect(err2).to.have.property("stack").that.not.contains("throwAnError");
-            expect(err2).to.have.property("stack").that.contains("captureTheStackTrace1");
-            expect(err2).to.have.property("stack").that.contains("captureTheStackTrace2");
-            expect(err2).to.have.property("stack").that.contains("captureTheStackTrace3");
-            // and be on the first line
-            var lines = err2.stack.split("\n");
-            if (lines[0] === err2.toString()) {
-              // some environments add the toString of the error on the first line
-              lines.shift();
-            }
-            lines.forEach(function(l, i) {
-              expect(l).to[0 <= i && i <= 2 ? "to" : "not"].contain("captureTheStackTrace");
-            });
-          }
-        });
-      });
-    });
-  // });
-}));
+      try {
+        try {
+          throwAnError()
+        } catch (err1) {
+          captureTheStackTrace3(err1)
+          // noinspection ExceptionCaughtLocallyJS
+          throw err1
+        }
+      } catch (err2) {
+        testUtil.log('err.stack: %s', err2.stack)
+        err2.stack.must.be.truthy()
+        err2.stack.must.not.contain('createAnError')
+        err2.stack.must.not.contain('throwAnError')
+        err2.stack.must.contain('captureTheStackTrace1')
+        err2.stack.must.contain('captureTheStackTrace2')
+        err2.stack.must.contain('captureTheStackTrace3')
+        // and be on the first line
+        const lines = err2.stack.split('\n')
+        if (lines[0] === err2.toString()) {
+          // some environments add the toString of the error on the first line
+          lines.shift()
+        }
+        lines.forEach((l, i) => {
+          l.must[i >= 0 && i <= 2 ? 'to' : 'not'].contain('captureTheStackTrace')
+        })
+      }
+    })
+  })
+})
