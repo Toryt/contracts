439225a8fcfee59ac50ad7c04c3b070e4f07df12
Jan Dockx <jandockx@gmail.com>
2018-07-04 11:21:29 +0200

    IV/Contract: remove AMD, standardify, es-ify, use arrow functions where appropriate

--- src/IV/Contract.js
+++ src/IV/Contract.js
@@ -14,81 +14,72 @@
  limitations under the License.
  */
 
-(function(factory) {
-  "use strict";
+'use strict'
 
-  var dependencies = ["./../_private/util", "./ConditionError", "./AbstractContract",
-                      "./PreconditionViolation", "./PostconditionViolation", "./ExceptionConditionViolation"];
+const util = require('../_private/util')
+const ConditionError = require('./ConditionError')
+const AbstractContract = require('./AbstractContract')
+const PreconditionViolation = require('./PreconditionViolation')
+const PostconditionViolation = require('./PostconditionViolation')
+const ExceptionConditionViolation = require('./ExceptionConditionViolation')
 
-  if (typeof define === "function" && define.amd) {
-    define(dependencies, factory);
-  }
-  else if (typeof exports === "object") {
-    module.exports = factory.apply(undefined, dependencies.map(function(d) {return require(d);}));
-  }
-}(function(util, ConditionError, AbstractContract,
-           PreconditionViolation, PostconditionViolation, ExceptionConditionViolation) {
-  "use strict";
+/**
+ * The separation between AbstractContract and Contract is necessary to break a dependency
+ * cycle with ConditionError.
+ *
+ * @constructor
+ */
+function Contract (kwargs) {
+  util.pre(function () { return !!kwargs })
+  util.pre(function () { return !kwargs.pre || util.typeOf(kwargs.pre) === 'array' })
+  util.pre(function () { return !kwargs.post || util.typeOf(kwargs.post) === 'array' })
+  util.pre(function () { return !kwargs.exception || util.typeOf(kwargs.exception) === 'array' })
 
-  /**
-   * The separation between AbstractContract and Contract is necessary to break a dependency
-   * cycle with ConditionError.
-   *
-   * @constructor
-   */
-  function Contract(kwargs) {
-    util.pre(function() {return !!kwargs;});
-    util.pre(function() {return !kwargs.pre || util.typeOf(kwargs.pre) === "array";});
-    util.pre(function() {return !kwargs.post || util.typeOf(kwargs.post) === "array";});
-    util.pre(function() {return !kwargs.exception || util.typeOf(kwargs.exception) === "array";});
+  AbstractContract.apply(this, arguments)
+}
 
-    AbstractContract.apply(this, arguments);
-  }
+Contract.prototype = new AbstractContract(
+  {pre: [AbstractContract.falseCondition]},
+  AbstractContract.internalLocation
+)
+Contract.prototype.constructor = Contract
+Contract.prototype.implementation = function (implFunction) {
+  util.pre(this, function () { return implFunction && util.typeOf(implFunction) === 'function' })
 
-  Contract.prototype = new AbstractContract(
-    {pre: [AbstractContract.falseCondition]},
-    AbstractContract.internalLocation
-  );
-  Contract.prototype.constructor = Contract;
-  Contract.prototype.implementation = function(implFunction) {
-    util.pre(this, function() {return implFunction && util.typeOf(implFunction) === "function";});
+  const contract = this
+  const location = util.firstLocationOutsideLibrary()
 
-    var contract = this;
-    var location = util.firstLocationOutsideLibrary();
-
-    function contractFunction() {
-      // cfThis: the this of the contract function call
-      var cfThis = this; // jshint ignore:line
-      var extendedArgs = Array.prototype.slice.call(arguments);
-      PreconditionViolation.prototype.verifyAll(contractFunction, contract.pre, cfThis, arguments);
-      var result;
-      var exception;
-      try {
-        result = implFunction.apply(cfThis, arguments);
+  function contractFunction () {
+    // cfThis: the this of the contract function call
+    const cfThis = this // jshint ignore:line
+    const extendedArgs = Array.prototype.slice.call(arguments)
+    PreconditionViolation.prototype.verifyAll(contractFunction, contract.pre, cfThis, arguments)
+    let result
+    let exception
+    try {
+      result = implFunction.apply(cfThis, arguments)
+    } catch (exc) {
+      if (exc instanceof ConditionError) { // necessary to report only the deepest failure clearly
+        throw exc
       }
-      catch (exc) {
-        if (exc instanceof ConditionError) { // necessary to report only the deepest failure clearly
-          throw exc;
-        }
-        exception = exc;
-      }
-      extendedArgs.push(exception || result);
-      extendedArgs.push(contractFunction.bind(cfThis));
-      if (exception) {
-        ExceptionConditionViolation.prototype.verifyAll(contractFunction, contract.exception, cfThis, extendedArgs);
-        throw exception;
-      }
-      PostconditionViolation.prototype.verifyAll(contractFunction, contract.post, cfThis, extendedArgs);
-      return result;
+      exception = exc
     }
+    extendedArgs.push(exception || result)
+    extendedArgs.push(contractFunction.bind(cfThis))
+    if (exception) {
+      ExceptionConditionViolation.prototype.verifyAll(contractFunction, contract.exception, cfThis, extendedArgs)
+      throw exception
+    }
+    PostconditionViolation.prototype.verifyAll(contractFunction, contract.post, cfThis, extendedArgs)
+    return result
+  }
 
-    AbstractContract.bless(contractFunction, contract, implFunction, location);
+  AbstractContract.bless(contractFunction, contract, implFunction, location)
 
-    return contractFunction;
-  };
+  return contractFunction
+}
 
-  Contract.root = AbstractContract.root;
-  Contract.isAContractFunction = AbstractContract.isAContractFunction;
+Contract.root = AbstractContract.root
+Contract.isAContractFunction = AbstractContract.isAContractFunction
 
-  return Contract;
-}));
+module.exports = Contract
