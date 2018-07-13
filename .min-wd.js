af0bfc5eba1d7484b9dc4b373eb06fa0250a955c
Jan Dockx <jandockx@gmail.com>
2018-07-13 14:11:32 +0200

    use dot reporting in wd tests, revert all to no async_polling
    on the idea that giving feedback 'keeps the lines open', and avoids some timeouts
    on the impression that no async_polling tests was more stable, reverting them all to this
    
    with the current setup, it took on average over 5 attempts to get all tests run

--- .min-wd.js
+++ .min-wd.js
@@ -1,8 +1,7 @@
 /* Note: Firefox tests work up until version 58. After that, Selenium has a problem:
          POST /session/14641de4c7f12c2f9b2eaf307a0955edf23dc59b/timeouts/async_script did not match a known command
          No combination of browserstack.geckodriver and browserstack.selenium_version that worked was found.
-         The solution is to call mochify with `--async-polling false`.
-         Firefox definitions are in a separate file, to make a separate mochify call possible */
+         The solution is to call mochify with `--async-polling false`. */
 
 const travisBuild = process.env.TRAVIS_BUILD_NUMBER
 const build = travisBuild
@@ -27,14 +26,19 @@ const osVersion = {
    By running the tests every week, we stay compatible. */
 
 const desktop = [
-  {browser: 'Firefox', os: 'OS X'}, // NOTE initially ok, with `--async-polling false`
-  {browser: 'Firefox', os: 'Windows'} // NOTE initially ok, with `--async-polling false`
+  {browser: 'Chrome', os: 'OS X', os_version: 'High Sierra'}, // NOTE initially ok
+  {browser: 'Safari', os: 'OS X', os_version: 'High Sierra'}, // NOTE initially ok, with exception of stack trace being bogus in Web Driver mode, and skipping frames in general
+  {browser: 'Firefox', os: 'OS X', os_version: 'High Sierra'}, // NOTE initially ok, with `--async-polling false`
+  {browser: 'Chrome', os: 'Windows', os_version: '10'}, // NOTE initially ok
+  {browser: 'Edge', os: 'Windows', os_version: '10'}, // NOTE initially ok, with exception of contract function name (test workaround)
+  {browser: 'Firefox', os: 'Windows', os_version: '10'} // NOTE initially ok, with `--async-polling false`
 ]
   .map(d => ({
     name: `${d.browser} - ${d.os}`,
     capabilities: Object.assign(d, capabilitiesBase, {os_version: osVersion[d.os]})
   }))
 const mobile = [
+  'Samsung Galaxy S9', // NOTE initially ok
   'Samsung Galaxy Note 4', // NOTE initially ok
   'iPhone X', // NOTE initially ok, with exception of stack trace being bogus in Web Driver mode, and skipping frames in general
   'iPad Pro' // NOTE initially ok, with exception of stack trace being bogus in Web Driver mode, and skipping frames in general
