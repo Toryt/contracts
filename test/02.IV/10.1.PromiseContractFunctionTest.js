545ebe2dccb82821e069fa6218240a47f5c33091
Jan Dockx <jandockx@gmail.com>
2018-07-15 22:41:07 +0200

    remove resolved MUDO

--- test/02.IV/10.1.PromiseContractFunctionTest.js
+++ test/02.IV/10.1.PromiseContractFunctionTest.js
@@ -523,8 +523,6 @@ describe('IV/PromiseContractFunction', function () {
 
   describe('postcondition', function () {
     describe('violation', function () {
-      // MUDO not if verifyPostconditions === false x 2
-
       it('fails when a simple postcondition is violated', function () {
         fibonacciWrong.contract.verifyPostconditions = true
         return callAndExpectRejection(undefined, fibonacciWrong, wrongParameter, expectPostProperties.bind(undefined, undefined, fibonacciWrong))
