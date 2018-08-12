0ec0411076b3ae5da0b3ff2ed3cb1a8df3d450eb
Jan Dockx <jandockx@gmail.com>
2018-08-12 20:38:09 +0200

    replace property.js with property.ts

---
+++ lib/_private/property.ts
@@ -0,0 +1,88 @@
+/*
+  Copyright 2016 - 2018 by Jan Dockx
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+'use strict'
+
+import { primitive as isPrimitive } from './is'
+import * as assert from 'assert'
+
+export function setAndFreeze(obj: object, propName: string, value: any): void {
+  assert.ok(!isPrimitive(obj))
+  assert.equal(typeof propName, 'string')
+
+  Object.defineProperty(obj, propName, {
+    configurable: false,
+    enumerable: true,
+    writable: false,
+    value: value
+  })
+}
+
+export function configurableDerived(
+  prototype: object,
+  propertyName: string,
+  derivation: () => any
+): void {
+  assert.ok(prototype)
+  assert.ok(!isPrimitive(prototype))
+  assert.equal(typeof propertyName, 'string')
+  assert.equal(typeof derivation, 'function')
+
+  Object.defineProperty(prototype, propertyName, {
+    configurable: true,
+    enumerable: true,
+    get: derivation,
+    set: undefined
+  })
+}
+
+export function frozenDerived(
+  prototype: object,
+  propertyName: string,
+  derivation: () => any
+): void {
+  assert.ok(prototype)
+  assert.ok(!isPrimitive(prototype))
+  assert.equal(typeof propertyName, 'string')
+  assert.equal(typeof derivation, 'function')
+
+  Object.defineProperty(prototype, propertyName, {
+    configurable: false,
+    enumerable: true,
+    get: derivation,
+    set: undefined
+  })
+}
+
+export interface ArrayPropertyHolder {
+  [key: string]: any
+}
+
+export function frozenReadOnlyArray(
+  prototype: object,
+  propertyName: string,
+  privatePropName: string
+): void {
+  assert.ok(prototype)
+  assert.ok(!isPrimitive(prototype))
+  assert.equal(typeof propertyName, 'string')
+  assert.equal(typeof privatePropName, 'string')
+  assert.notEqual(propertyName, privatePropName)
+
+  frozenDerived(prototype, propertyName, function(this: ArrayPropertyHolder) {
+    return this[privatePropName].slice()
+  })
+}
