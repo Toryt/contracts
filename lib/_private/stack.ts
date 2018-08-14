d93d6504eea162babb14741b7c2bf64ed9c339a3
Jan Dockx <jandockx@gmail.com>
2018-08-14 20:17:03 +0200

    change how optional arguments are represented

--- lib/_private/stack.ts
+++ lib/_private/stack.ts
@@ -51,7 +51,7 @@ export const skipsForEach: boolean = (function(): boolean {
  * Note that in Safari, the result cannot be trusted. Safari skips (optimized?) stack frames. In Safari, this
  * will return 'a' stack line, but not necessarily a semantic meaningful one.
  */
-export function location(depth: number | undefined): string {
+export function location(depth?: number): string {
   assert(!depth || Number.isInteger(depth), 'optional depth is an integer')
   assert(!depth || depth >= 0, 'optional depth is positive')
 
@@ -87,7 +87,7 @@ export function location(depth: number | undefined): string {
  * Note that in Safari, the result cannot be trusted. Safari skips (optimized?) stack frames. In Safari, this
  * will return 'a' stack, but not necessarily a semantic meaningful one.
  */
-export function raw(depth: number | undefined): string {
+export function raw(depth?: number): string {
   assert(!depth || Number.isInteger(depth), 'optional depth is an integer')
   assert(!depth || depth >= 0, 'optional depth is positive')
 
