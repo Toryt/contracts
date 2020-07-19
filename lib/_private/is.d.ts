8e645952d44a15c573e35e0cc7457f1b5384d8ae
Jan Dockx <jandockx@gmail.com>
2020-07-19 12:41:50 +0200

    add this: never to is functions

--- lib/_private/is.d.ts
+++ lib/_private/is.d.ts
@@ -1,11 +1,11 @@
-export declare function functionArguments(a: any): a is IArguments;
+export declare function functionArguments(this:never, a: any): a is IArguments;
 
-export declare function primitive(p: any): p is number | string | boolean;
+export declare function primitive(this:never, p: any): p is number | string | boolean;
 
 export type StackLocation = string;
-export declare function stackLocation(location: any): location is StackLocation;
+export declare function stackLocation(this:never, location: any): location is StackLocation;
 
 export type Stack = string;
-export declare function stack(stack: any): stack is Stack;
+export declare function stack(this:never, stack: any): stack is Stack;
 
-export declare function frozenOwnProperty(obj: object, propName: string): boolean;
+export declare function frozenOwnProperty(this:never, obj: object, propName: string): boolean;
