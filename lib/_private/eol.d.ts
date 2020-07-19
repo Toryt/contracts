10777158e827b8c5c468c3ea3ae12c95b3c3bd2f
Jan Dockx <jandockx@gmail.com>
2020-07-19 12:14:29 +0200

    add d.ts for eol.js

--- lib/_private/eol.d.ts
+++ lib/_private/eol.d.ts
@@ -1,8 +1,10 @@
+export type EOL = "\n" | "\r\n";
+
 export const n: "\n";
 export const rn: "\r\n";
 /**
  * Determine the EOL used by this JavaScript engine in Error stack traces. It turns out this is not always `os.EOL`.
  * The default is '\n' (Unix).
  */
-export const stack: "\n" | "\r\n";
-export const os: string;
+export const stack: EOL;
+export const os: EOL;
