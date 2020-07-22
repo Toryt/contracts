d0f52080a6914895e1bc9f2865ada2eca2b95cc1
Jan Dockx <jandockx@gmail.com>
2020-07-22 21:59:14 +0200

    make ContractError test TS
    the stack getter is not called!

---
+++ test/02.ContractErrorTest.ts
@@ -0,0 +1,57 @@
+/*
+ Copyright 2016 - 2020 by Jan Dockx
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+/* eslint-env mocha */
+
+import "should";
+import {raw} from "../lib/_private/stack";
+import ContractError from "../lib/ContractError";
+import {frozenDerived} from "../lib/_private/property";
+import {log} from "./_util/testUtil";
+import {expectConstructorPost, expectInvariants, generatePrototypeMethodsDescriptions} from "./ContractErrorCommon";
+
+describe('ContractError', function () {
+  describe('#ContractError()', function () {
+    it('creates an instance with all toppings', function () {
+      const rawStack = raw();
+      const result = new ContractError(rawStack);
+      log('result:\n%s', result);
+      log('result.toString():\n%s', result.toString());
+      log('result.stack:\n%s', result.stack);
+      expectConstructorPost(result, ContractError.message, rawStack);
+      expectInvariants(result);
+      result.should.not.have.ownProperty('message');
+      log('result.stack:\n%s', result.stack);
+    });
+    it('can get a message set', function () {
+      const result = new ContractError(raw());
+      const message = 'another message';
+      frozenDerived(result, 'message', function () {
+        return message;
+      });
+      result.should.have.ownProperty('message');
+      result.message.should.equal(message);
+      expectInvariants(result);
+    });
+  });
+
+  generatePrototypeMethodsDescriptions(() => new ContractError(raw()), [
+    {
+      subject: () => new ContractError(raw()),
+      description: 'a contract error'
+    }
+  ]);
+});
---
+++ test/ContractErrorCommon.ts
@@ -0,0 +1,70 @@
+/*
+ Copyright 2016 - 2020 by Jan Dockx
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+/* eslint-env mocha */
+
+import type {Stack} from "../lib/_private/is";
+import {stack as stackEOL} from "../lib/_private/eol";
+import {stack as isStack} from "../lib/_private/is";
+import ContractError from "../lib/ContractError";
+import {expectOwnFrozenProperty, log, regExpEscape} from "./_util/testUtil";
+
+export function expectStackInvariants (subject: ContractError) {
+  const stack: Stack = subject.stack;
+  log(`subject stack: ${stack}`);
+  stack.should.be.a.String();
+  const startOfStack = subject.name + ': ' + subject.message + stackEOL;
+  stack.should.match(new RegExp('^' + regExpEscape(startOfStack)));
+  const restOfStack: Stack = stack
+    .replace(startOfStack, '')
+    .split(stackEOL)
+    // remove empty lines that may occur in 'caused by' in Safari when used via Web Driver
+    .filter(l => !!l)
+    .join(stackEOL);
+  isStack(restOfStack).should.be.true();
+  log(`rest of stack: ${restOfStack}`);
+  log(`raw stack: ${subject._rawStack}`);
+  // noinspection JSUnresolvedVariable
+  restOfStack.should.containEql(subject._rawStack);
+}
+
+export function expectInvariants (subject: ContractError) {
+  subject.should.be.an.instanceof(ContractError);
+  expectOwnFrozenProperty(Object.getPrototypeOf(subject), 'name');
+  subject.name.should.be.a.String();
+  subject.name.should.equal(subject.constructor.name);
+  expectOwnFrozenProperty(ContractError.prototype, 'message');
+  subject.message.should.be.a.String();
+  expectOwnFrozenProperty(subject, '_rawStack');
+  // noinspection JSUnresolvedVariable
+  isStack(subject._rawStack).should.be.true();
+  expectStackInvariants(subject);
+}
+
+export function expectConstructorPost (result: ContractError, message: string, rawStack: Stack): void {
+  Object.isExtensible(result).should.be.true();
+  result.name.should.equal(result.constructor.name);
+  result.message.should.equal(message);
+  result._rawStack.should.equal(rawStack);
+}
+
+// noinspection FunctionNamingConventionJS
+export function generatePrototypeMethodsDescriptions (
+  _oneSubjectGenerator: () => ContractError,
+  _allSubjectGenerators: Array<{ subject: () => ContractError, description: string }>
+): void {
+  // NOP: no methods here
+}
