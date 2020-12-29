93ca56da60b9aa50e69ef9fcd2f8b0761762c007
Jan Dockx <jandockx@gmail.com>
2020-12-29 20:57:25 +0100

    Test that Any…Function behaves as expected - apart from 3 MUDOs

--- test-d/AnyFunction.test-d.ts
+++ test-d/AnyFunction.test-d.ts
@@ -1,5 +1,5 @@
 /*
- Copyright 2016 - 2020 by Jan Dockx
+ Copyright 2020 - 2020 by Jan Dockx
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -14,57 +14,114 @@
   limitations under the License.
  */
 
-import { expectAssignable, expectError, expectNotType, expectType } from 'tsd'
-import { AnyFunction } from '../types'
+import { expectAssignable, expectError, expectNotAssignable, expectNotType, expectType } from 'tsd'
+import { AnyCallableFunction, AnyFunction, AnyNewableFunction } from '../types'
 
 interface SomeObject {
   aProperty: number
 }
 
 type AFunction1 = (this: SomeObject, a: string, b: number) => string
-
 function aFunction1 (this: SomeObject, a: string, b: number): string { return this.aProperty + a + b }
 expectType<AFunction1>(aFunction1)
+expectAssignable<AnyCallableFunction>(aFunction1)
+expectNotAssignable<AnyNewableFunction>(aFunction1)
 expectAssignable<AnyFunction>(aFunction1)
+expectType<number>(aFunction1.length)
+expectType<Function['toString']>(aFunction1.toString)
+expectType<CallableFunction['apply']>(aFunction1.apply)
+expectType<CallableFunction['call']>(aFunction1.call)
+expectType<CallableFunction['bind']>(aFunction1.bind)
+// `prototype` is defined with type `any` in `lib.es5.d.ts`, and there is nothing we can do about that … (***)
+expectType<any>(aFunction1.prototype)
 
 type AFunction2 = (a: string, b: number) => string
 const aFunction2: AFunction2 = function aFunction2 (a, b) { return `${a}${b} more text` }
 expectType<AFunction2>(aFunction2)
 expectAssignable<AFunction1>(aFunction2)
+expectAssignable<AnyCallableFunction>(aFunction2)
+expectNotAssignable<AnyNewableFunction>(aFunction2)
 expectAssignable<AnyFunction>(aFunction2)
+expectType<number>(aFunction2.length)
+expectType<Function['toString']>(aFunction2.toString)
+expectType<CallableFunction['apply']>(aFunction2.apply)
+expectType<CallableFunction['call']>(aFunction2.call)
+expectType<CallableFunction['bind']>(aFunction1.bind)
+// `prototype` is defined with type `any` in `lib.es5.d.ts`, and there is nothing we can do about that … (***)
+expectType<any>(aFunction2.prototype)
 
 type AFunction3 = (a: string) => 'truth' | 'dare'
 const aFunction3: AFunction3 = function aFunction3 (a) { return a > 'm' ? 'truth' : 'dare' }
 expectType<AFunction3>(aFunction3)
 expectAssignable<AFunction2>(aFunction3)
 expectAssignable<AFunction1>(aFunction3)
+expectAssignable<AnyCallableFunction>(aFunction3)
+expectNotAssignable<AnyNewableFunction>(aFunction3)
 expectAssignable<AnyFunction>(aFunction3)
-
-let anyFunction: AnyFunction = aFunction1
-expectType<AnyFunction>(anyFunction)
-expectType<CallableFunction['apply']>(anyFunction.apply)
-expectType<CallableFunction['call']>(anyFunction.call)
-// MUDO expectType<CallableFunction['bind']>(anyFunction.call)
+expectType<number>(aFunction3.length)
+expectType<Function['toString']>(aFunction3.toString)
+expectType<CallableFunction['apply']>(aFunction3.apply)
+expectType<CallableFunction['call']>(aFunction3.call)
+expectType<CallableFunction['bind']>(aFunction1.bind)
 // `prototype` is defined with type `any` in `lib.es5.d.ts`, and there is nothing we can do about that … (***)
-expectType<any>(anyFunction.prototype)
-expectType<Function['toString']>(anyFunction.toString)
-expectType<number>(anyFunction.length)
+expectType<any>(aFunction3.prototype)
 
-class A {
+let anyCallableFunction: AnyCallableFunction = aFunction1
+expectType<AnyCallableFunction>(anyCallableFunction)
+expectAssignable<AnyFunction>(anyCallableFunction)
+expectType<number>(anyCallableFunction.length)
+expectType<Function['toString']>(anyCallableFunction.toString)
+expectType<CallableFunction['apply']>(anyCallableFunction.apply)
+expectType<CallableFunction['call']>(anyCallableFunction.call)
+expectType<CallableFunction['bind']>(anyCallableFunction.bind)
+// `prototype` is defined with type `any` in `lib.es5.d.ts`, and there is nothing we can do about that … (***)
+expectType<any>(anyCallableFunction.prototype)
+
+// without `as AnyFunction`, TS still sees this as AnyCallableFunction
+let anyFunction1a: AnyFunction = aFunction1
+expectAssignable<AnyFunction>(anyFunction1a)
+expectNotType<AnyNewableFunction>(anyFunction1a)
+expectType<AnyCallableFunction>(anyFunction1a)
+
+let anyFunction1b: AnyFunction = aFunction1 as AnyFunction
+expectType<AnyFunction>(anyFunction1b)
+expectType<number>(anyFunction1b.length)
+expectType<Function['toString']>(anyFunction1b.toString)
+// MUDO investigate: expectType<CallableFunction['apply']>(anyFunction1b.apply)
+// MUDO investigate: expectType<CallableFunction['call']>(anyFunction1b.call)
+// MUDO investigate: expectType<CallableFunction['bind']>(anyFunction1.bind)
+// `prototype` is defined with type `any` in `lib.es5.d.ts`, and there is nothing we can do about that … (***)
+expectType<any>(anyFunction1b.prototype)
+
+class ANewableFunction {
   constructor(a: string, b: number) {}
 }
-expectType<typeof A>(A)
-expectAssignable<new (a: string, b: number) => A>(A)
-expectAssignable<AnyFunction>(A)
+expectType<typeof ANewableFunction>(ANewableFunction)
+expectAssignable<new (a: string, b: number) => ANewableFunction>(ANewableFunction)
+expectNotAssignable<AnyCallableFunction>(ANewableFunction)
+expectAssignable<AnyNewableFunction>(ANewableFunction)
+expectAssignable<AnyFunction>(ANewableFunction)
+expectType<number>(ANewableFunction.length)
+expectType<Function['toString']>(anyCallableFunction.toString)
+// MUDO investigate: expectType<NewableFunction['apply']>(anyCallableFunction.apply)
+// MUDO investigate: expectType<NewableFunction['call']>(anyCallableFunction.call)
+// MUDO investigate: expectType<NewableFunction['bind']>(anyCallableFunction.bind)
 // (***) … but TS knows that the prototype of a constructor is the type of the class
-expectType<A>(A.prototype)
+expectType<ANewableFunction>(ANewableFunction.prototype)
 
-let anyConstructor: AnyFunction = A
-expectType<AnyFunction>(anyConstructor)
-expectError<CallableFunction['apply']>(anyConstructor.apply) // MUDO this fails; why? the static type is the same as above
-expectType<NewableFunction['apply']>(anyConstructor.apply)
-expectType<NewableFunction['call']>(anyConstructor.call)
-// MUDO expectType<NewableFunction['bind']>(anyConstructor.call)
-expectType<any>(anyConstructor.prototype)
-expectType<Function['toString']>(anyConstructor.toString)
-expectType<number>(anyConstructor.length)
+// without `as AnyFunction`, TS still sees this as AnyCallableFunction
+let anyFunction2a: AnyFunction = ANewableFunction
+expectNotType<AnyFunction>(anyFunction2a)
+expectAssignable<AnyFunction>(anyFunction2a)
+expectNotType<AnyCallableFunction>(anyFunction2a)
+expectType<AnyNewableFunction>(anyFunction2a)
+
+let anyFunction2b: AnyFunction = ANewableFunction as AnyFunction
+expectType<AnyFunction>(anyFunction2b)
+expectType<number>(anyFunction1b.length)
+expectType<Function['toString']>(anyFunction2b.toString)
+// MUDO investigate: expectType<CallableFunction['apply']>(anyFunction2b.apply)
+// MUDO investigate: expectType<CallableFunction['call']>(anyFunction2b.call)
+// MUDO investigate: expectType<CallableFunction['bind']>(anyFunction2b.bind)
+// `prototype` is defined with type `any` in `lib.es5.d.ts`, and there is nothing we can do about that … (***)
+expectType<any>(anyFunction2b.prototype)
