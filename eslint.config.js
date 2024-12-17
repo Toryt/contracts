c8fc3a6cd4d713a34d3036e1e23ec59542ed2fc3
Jan Dockx <jandockx@gmail.com>
2024-12-17 19:11:07 +0100

    completely upgrade linting and formatting settings

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const neostandard = require('neostandard')
+const { mocha } = require('globals')
+const stylistic = neostandard.plugins['@stylistic']
+const depend = require('eslint-plugin-depend')
+const json = require('eslint-plugin-json')
+const noSecrets = require('eslint-plugin-no-secrets')
+
+module.exports = neostandard({}).concat([
+  {
+    name: 'prettier-overrides',
+    plugins: { '@stylistic': stylistic },
+    rules: {
+      '@stylistic/space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'always',
+          named: 'never',
+          asyncArrow: 'always'
+        }
+      ]
+    }
+  },
+  {
+    name: 'mocha-globals',
+    files: [
+      'test/{*,**/*}Test.js',
+    ],
+    languageOptions: {
+      globals: {
+        ...mocha
+      }
+    }
+  },
+  depend.configs['flat/recommended'],
+  {
+    files: ['**/*.json'],
+    ...json.configs['recommended']
+  },
+  {
+    files: ['**/*.js'],
+    plugins: {
+      'no-secrets': noSecrets
+    },
+    rules: {
+      'no-secrets/no-secrets': ['error']
+    }
+  }
+])
