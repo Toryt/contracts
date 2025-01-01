b7dbe3b96405f09b8e7711c109939a7ced239367
Jan Dockx <jandockx@gmail.com>
2025-01-01 22:25:02 +0100

    expand UnknownFunction tests
    [skip ci]

--- test-d/src/types/UnknownFunction.test-d.ts
+++ test-d/src/types/UnknownFunction.test-d.ts
@@ -1,5 +1,5 @@
 /*
-  Copyright 2024 Jan Dockx
+  Copyright 2024–2025 Jan Dockx
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -16,6 +16,31 @@
 
 import { expectAssignable } from 'tsd'
 import type { UnknownFunction } from '../../../src/index.ts'
+import type {
+  ASignature,
+  ASignatureWithOptionalArgs,
+  DoubleOptionalAfterRestSignature,
+  DoubleOptionalBeforeRestSignature,
+  FinalOptionalArgumentSignature,
+  FinalRestArgumentSignature,
+  MultipleFinalOptionalArgumentsSignature,
+  NoArgumentsSignature,
+  OneArgumentSignature,
+  OneRestInTheMiddleInArraysSignature,
+  OptionalAfterRestSignature,
+  OptionalBeforeRestSignature,
+  PseudoOptionalNonFinalSignature,
+  PseudoRestNonFinalSignature,
+  SingleOptionalArgumentSignature,
+  SingleRestSignature,
+  TwoArgumentsSignature,
+  UndefinedBeforeRestSignature,
+  UndefinedNonFinalSignature
+} from '../../../test2/util/SomeSignatures.ts'
+
+function unknownFunction(): unknown {
+  return undefined
+}
 
 expectAssignable<UnknownFunction>((a: number, b: number): number => a + b)
 expectAssignable<UnknownFunction>((a: number): number => a)
@@ -27,3 +52,23 @@ expectAssignable<UnknownFunction>((a: number, b: unknown): never => {
 })
 expectAssignable<UnknownFunction>((a: number, b: number): string => `${a + b}`)
 expectAssignable<UnknownFunction>((a: number, b: number): unknown => a | b)
+
+expectAssignable<UnknownFunction>(unknownFunction as unknown as NoArgumentsSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as OneArgumentSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as TwoArgumentsSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as FinalOptionalArgumentSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as SingleOptionalArgumentSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as MultipleFinalOptionalArgumentsSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as FinalRestArgumentSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as SingleRestSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as PseudoOptionalNonFinalSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as UndefinedNonFinalSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as PseudoRestNonFinalSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as OneRestInTheMiddleInArraysSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as OptionalBeforeRestSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as DoubleOptionalBeforeRestSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as UndefinedBeforeRestSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as OptionalAfterRestSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as DoubleOptionalAfterRestSignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as ASignature)
+expectAssignable<UnknownFunction>(unknownFunction as unknown as ASignatureWithOptionalArgs)
