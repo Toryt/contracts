3a6c7c95a6d117195e33cf9ef2ed1b344b7a0b0d
Jan Dockx <jandockx@gmail.com>
2025-01-05 04:26:14 +0100

    add eol as TS and test

---
+++ src/private/eol.ts
@@ -0,0 +1,34 @@
+/*
+  Copyright 2019–2025 Jan Dockx
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+export { EOL as os } from 'os'
+
+export type EOL = '\n' | '\r\n'
+
+export const n = '\n'
+export const rn = '\r\n'
+
+const message = 'STACK MESSAGE'
+const stackError = new Error(message)
+
+/**
+ * Determine the EOL used by this JavaScript engine in Error stack traces. It turns out this is not always `os.EOL`.
+ * The default is '\n' (Unix).
+ */
+export const stack: EOL =
+  'stack' in stackError && stackError.stack.startsWith(message + rn)
+    ? /* istanbul ignore next: platform dependent */ rn
+    : n
