a650b8b94761312bdd5617e9b625535efe956b33
Jan Dockx <jandockx@gmail.com>
2025-01-05 12:06:02 +0100

    convert is to TS and test

---
+++ src/private/is.ts
@@ -0,0 +1,80 @@
+/*
+  Copyright 2016–2025 Jan Dockx
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+import { rn as rnEOL, n as nEOL, stack as stackEOL } from './eol.ts'
+import { notStrictEqual } from 'assert'
+
+const anArgumentsToString: string = (function () {
+  return Object.prototype.toString.call(arguments)
+})()
+
+// MUDO rename functions to `is…`
+
+export function functionArguments(a: unknown): a is IArguments {
+  /* NOTE: It is hard to determine whether something is an `arguments`, and it has become harder still since we have
+             Symbols. This solution is derived from
+             https://stackoverflow.com/questions/7656280/how-do-i-check-whether-an-object-is-an-arguments-object-in-javascript/7656333#7656333 */
+  return Object.prototype.toString.call(a) === anArgumentsToString
+}
+
+/**
+ * p is a true primitive, i.e., not null, undefined, an object (which implies, not a Date, Math or JSON, nor any
+ * Error, and not an array or arguments, and wrapped primitives), not a function. p is a true string, number or
+ * boolean.
+ */
+export function primitive(p: unknown): p is number | string | boolean {
+  return p !== null && ['number', 'string', 'boolean'].indexOf(typeof p) >= 0
+}
+
+/**
+ * <code>location</code> is a stack line location.
+ *
+ * Over Node, cross-platform, and different browsers, we can only say this has to be a none-empty string, that is not
+ * multi-line.
+ *
+ * It does not always end with a line number and column number (native code), it does not always start with 'at'
+ * (Firefox), …
+ */
+export function stackLocation(location: unknown): location is string {
+  return !!location && typeof location === 'string' && location.indexOf(rnEOL) < 0 && location.indexOf(nEOL) < 0
+}
+
+/**
+ * <code>stack</code> is a stack.
+ *
+ * Over Node, cross-platform, and different browsers, we can only say this has to be a none-empty, multi-line string,
+ * with at least 1 line, and no empty lines.
+ *
+ * Lines do not always end with a line number and column number (native code), do not always start with 'at'
+ * (Firefox), …
+ */
+export function stack(stack: unknown): stack is string {
+  const lines = !!stack && typeof stack === 'string' && stack.split(stackEOL)
+  return lines && lines.length > 0 && lines.every(l => stackLocation(l))
+}
+
+export function frozenOwnProperty(obj: NonNullable<object>, propName: string): boolean | undefined {
+  notStrictEqual(obj, null)
+  notStrictEqual(obj, undefined)
+
+  const descriptor = Object.getOwnPropertyDescriptor(obj, propName)
+  return (
+    descriptor &&
+    descriptor.enumerable === true &&
+    descriptor.configurable === false &&
+    (descriptor.writable === false || (typeof descriptor.get === 'function' && !descriptor.set))
+  )
+}
---
+++ test2/js/private/01.isTest.js
@@ -0,0 +1,274 @@
+/*
+  Copyright 2015–2025 Jan Dockx
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+import should from 'should'
+import { functionArguments, primitive, stackLocation, stack, frozenOwnProperty } from '../../../build/src/private/is.js'
+import { n, rn, stack as stackEOL } from '../../../build/src/private/eol.js'
+import { notStackEOL } from '../../../build/test2/util/cases.js'
+import { generateStuff } from '../../../build/test2/util/_stuff.js'
+import { x, log, safeToString, showStack } from '../../../build/test2/util/testUtil.js'
+
+describe('_private/is', function () {
+  describe('#arguments', function () {
+    generateStuff().forEach(s => {
+      it(`returns ${s.expected === 'arguments' ? 'true' : 'false'} for ${safeToString(s.subject)}`, function () {
+        const result = functionArguments(s.subject)
+        if (s.expected === 'arguments') {
+          result.should.be.true()
+        } else {
+          should(result).not.be.ok()
+        }
+      })
+    })
+  })
+
+  describe('#primitive()', function () {
+    generateStuff().forEach(record => {
+      it(`correctly decides whether the argument is a primitive for ${safeToString(record.subject)}`, function () {
+        const result = primitive(record.subject)
+        result.should.be.a.Boolean()
+        result.should.equal(record.isPrimitive)
+      })
+    })
+  })
+
+  describe('#stackLocation', function () {
+    generateStuff()
+      .map(s => s.subject)
+      .filter(s => typeof s !== 'string')
+      .forEach(s => {
+        it(`says no to ${safeToString(s)}`, function () {
+          const result = stackLocation(s)
+          result.should.be.false()
+        })
+      })
+    it("says no to ''", function () {
+      const result = stackLocation('')
+      result.should.be.false()
+    })
+    it("says yes to 'abc'", function () {
+      const result = stackLocation('abc')
+      result.should.be.true()
+    })
+    it('says no to a multi-line string with \\n as EOL', function () {
+      // do not use a multi-line template string: the EOLs in the source code (\n) are recorded, and then the test fails
+      // on Windows
+      const result = stackLocation('this is a' + n + 'multi-line' + n + 'string')
+      result.should.be.false()
+    })
+    it('says no to a multi-line string with \\r\\n as EOL', function () {
+      // do not use a multi-line template string: the EOLs in the source code (\n) are recorded, and then the test fails
+      // on Windows
+      const result = stackLocation('this is a' + rn + 'multi-line' + rn + 'string')
+      result.should.be.false()
+    })
+    it('says yes to all lines of a stack trace', function () {
+      // sadly, also to the message
+      const error = new Error('This is an error to get a platform dependent stack')
+      const lines = error.stack.split(stackEOL)
+      lines
+        .filter((line, index) => index !== lines.length - 1 || line.length > 0) // FF adds an empty line at the end
+        .filter(line => line.length > 0) // Safari has lots of empty lines, but only when used remotely (with WebDriver)
+        .forEach(line => {
+          const result = stackLocation(line)
+          log(`${result}: ${line}`)
+          result.should.be.true()
+        })
+    })
+  })
+
+  describe('#stack', function () {
+    generateStuff()
+      .map(s => s.subject)
+      .filter(s => typeof s !== 'string')
+      .forEach(s => {
+        it(`says no to ${safeToString(s)}`, function () {
+          const result = stack(s)
+          result.should.be.false()
+        })
+      })
+    it("says no to ''", function () {
+      const result = stack('')
+      result.should.be.false()
+    })
+    it("says yes to 'abc'", function () {
+      const result = stack('abc')
+      result.should.be.true()
+    })
+    it('says yes to a multi-line string with `eol.stack`', function () {
+      // do not use a multi-line template string: the EOLs in the source code (\n) are recorded, and then the test fails
+      // on Windows
+      const candidate = 'this is a' + stackEOL + 'multi-line' + stackEOL + 'string'
+      const result = stack(candidate)
+      result.should.be.true()
+    })
+    it('says no to a multi-line string with the other EOL', function () {
+      // do not use a multi-line template string: the EOLs in the source code (\n) are recorded, and then the test fails
+      // on Windows
+      const candidate = 'this is a' + notStackEOL + 'multi-line' + notStackEOL + 'string'
+      const result = stack(candidate)
+      result.should.be.true()
+    })
+    it('says no to a multi-line string with a blank line with `eol.stack`', function () {
+      // do not use a multi-line template string: the EOLs in the source code (\n) are recorded, and then the test fails
+      // on Windows
+      const result = stack(
+        'this is a' + stackEOL + 'multi-line' + stackEOL + 'string, with a' + stackEOL + stackEOL + 'blank line'
+      )
+      result.should.be.false()
+    })
+    it('says yes to a multi-line string with a blank line with other EOL (looks like a single line)', function () {
+      // do not use a multi-line template string: the EOLs in the source code (\n) are recorded, and then the test fails
+      // on Windows
+      const result = stack(
+        'this is a' +
+          notStackEOL +
+          'multi-line' +
+          notStackEOL +
+          'string, with a' +
+          notStackEOL +
+          notStackEOL +
+          'blank line'
+      )
+      // with the other EOL, it looks like a single line, which is a good stack
+      result.should.be.true()
+    })
+    it('says yes to a stack trace', function () {
+      const message = 'This is an error to get a platform dependent stack'
+      // sadly, also to the message, on some platforms
+      const error = new Error(message)
+      showStack(error)
+      const stackLines = error.stack.split(stackEOL)
+      const rawStack = stackLines
+        // remove message line
+        .filter(sl => sl && sl.indexOf(message) < 0)
+        .join(stackEOL)
+      log('rawStack:')
+      log(`▷${rawStack}◁`)
+      const result = stack(rawStack)
+      result.should.be.true()
+    })
+  })
+
+  describe('#frozenOwnProperty()', function () {
+    const propName = 'test prop name'
+    const propValue = 'dummy value'
+    const truths = [true, false]
+    x(truths, truths, truths).forEach(values => {
+      const subject = {}
+      Object.defineProperty(subject, propName, {
+        configurable: values[0],
+        enumerable: values[1],
+        writable: values[2],
+        value: propValue
+      })
+      if (!values[0] && values[1] && !values[2] && Object.prototype.hasOwnProperty.call(subject, propName)) {
+        it(
+          'reports true if the property is an own property, ' +
+            'and it is enumerable, not configurable and not writable',
+          function () {
+            const result = frozenOwnProperty(subject, propName)
+            result.should.be.ok()
+          }
+        )
+      } else {
+        it(
+          'reports false if the property is an own property, and' +
+            ' enumerable === ' +
+            values[1] +
+            ' configurable === ' +
+            values[0] +
+            ' writable === ' +
+            values[2],
+          function () {
+            const result = frozenOwnProperty(subject, propName)
+            should(result).not.be.ok()
+          }
+        )
+      }
+      it('reports false if the property does not exist', function () {
+        const result = frozenOwnProperty(subject, 'some other, non-existing property name')
+        should(result).not.be.ok()
+      })
+      const specialized = {}
+      Object.setPrototypeOf(specialized, subject)
+      specialized[propName].should.equal(propValue) // check inheritance — test code validity
+      it(
+        'reports false if the property is not an own property, and' +
+          ' enumerable === ' +
+          values[1] +
+          ' configurable === ' +
+          values[0] +
+          ' writable === ' +
+          values[2],
+        function () {
+          const specializedResult = frozenOwnProperty(specialized, propName)
+          should(specializedResult).not.be.ok()
+        }
+      )
+    })
+    const notObjects = [0, false, '', 'lala']
+    notObjects.forEach(notAnObject => {
+      // cannot set a property on primitives
+      it(`reports false if the first parameter is a primitive (${typeof notAnObject})`, function () {
+        const result = frozenOwnProperty(notAnObject, propName)
+        should(result).not.be.ok()
+      })
+    })
+    const fCandidates = [undefined, function () {}]
+    x(truths, truths, fCandidates, fCandidates).forEach(values => {
+      const subject = {}
+      Object.defineProperty(subject, propName, {
+        configurable: values[0],
+        enumerable: values[1],
+        get: values[2],
+        set: values[3]
+      })
+      if (
+        !values[0] &&
+        values[1] &&
+        typeof values[2] === 'function' &&
+        values[3] === undefined &&
+        Object.prototype.hasOwnProperty.call(subject, propName)
+      ) {
+        it(
+          'reports true if the property is an own property, and it is enumerable, and not configurable, has a ' +
+            'getter, but not a setter',
+          function () {
+            const result = frozenOwnProperty(subject, propName)
+            result.should.be.ok()
+          }
+        )
+      } else {
+        it(
+          'reports false if the property is an own property,' +
+            ' enumerable === ' +
+            values[1] +
+            ' configurable === ' +
+            values[0] +
+            ' get === ' +
+            values[2] +
+            ' set === ' +
+            values[3],
+          function () {
+            const result = frozenOwnProperty(subject, propName)
+            should(result).not.be.ok()
+          }
+        )
+      }
+    })
+  })
+})
