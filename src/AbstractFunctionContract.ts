18f33cd55ebe043c5964567ab416955b19df5195
Jan Dockx <jandockx@gmail.com>
2025-01-10 23:19:31 +0100

    fix double import

--- src/AbstractFunctionContract.ts
+++ src/AbstractFunctionContract.ts
@@ -15,14 +15,13 @@
  */
 
 import { ContractError } from './ContractError.ts'
-import { frozenOwnProperty } from './private/is.ts'
 import type { UnknownFunction } from './types/UnknownFunction.ts'
 import type { Postcondition } from './Postcondition.ts'
 import assert, { ok, strictEqual } from 'assert'
 import { location as stackLocation } from './private/stack.ts'
 import { setAndFreeze } from './private/property.ts'
 import { namePrefix } from './private/report.ts'
-import { stack as isStack } from './private/is.ts'
+import { stack as isStack, frozenOwnProperty } from './private/is.ts'
 
 export const abstractErrorMessage = 'an abstract function cannot be executed'
 
