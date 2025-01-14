31ec7b9769e4fbc29fc9a9b55042b9f974855b63
Jan Dockx <jandockx@gmail.com>
2025-01-14 18:21:55 +0100

    mv isFrozenOwnProperty test

--- test2/ts/src/01.private/02.property.test.ts
+++ test2/ts/src/01.private/02.property.test.ts
@@ -14,12 +14,15 @@
   limitations under the License.
  */
 
+import { isFrozenOwnProperty } from '../../../../src/private/is.ts'
 import {
   configurableDerived,
   frozenDerived,
   frozenReadOnlyArray,
   setAndFreeze
 } from '../../../../src/private/property.ts'
+import { booleanStuff } from '../../../util/_stuff.ts'
+import { x } from '../../../util/cartesian.ts'
 import {
   expectConfigurableDerivedPropertyOnAPrototype,
   expectFrozenDerivedPropertyOnAPrototype,
@@ -132,6 +135,83 @@ describe(testName(import.meta), function () {
       const changedSubject = subject as typeof changedPrototype
       changedSubject[propertyName].should.not.equal(array)
       changedSubject[propertyName].should.eql(array)
+    })
+  })
+
+  describe('isFrozenOwnProperty()', function () {
+    const propName = 'test prop name'
+    const propValue = 'dummy value'
+    const truths = booleanStuff.map(({ subject }) => subject)
+    x(truths, truths, truths).forEach(([configurable, enumerable, writable]) => {
+      const subject = {}
+      Object.defineProperty(subject, propName, {
+        configurable,
+        enumerable,
+        writable,
+        value: propValue
+      })
+      if (!configurable && enumerable && !writable && Object.prototype.hasOwnProperty.call(subject, propName)) {
+        it('reports true if the property is an own property, and it is enumerable, not configurable and not writable', function () {
+          const result = isFrozenOwnProperty(subject, propName)
+          should(result).be.ok()
+        })
+      } else {
+        it(`reports false if the property is an own property, and enumerable === ${enumerable} configurable === ${configurable} writable === ${writable}`, function () {
+          const result = isFrozenOwnProperty(subject, propName)
+          should(result).not.be.ok()
+        })
+      }
+
+      it('reports false if the property does not exist', function () {
+        const result = isFrozenOwnProperty(subject, 'some other, non-existing property name')
+        should(result).not.be.ok()
+      })
+
+      const specialized: Record<string, string> = {}
+      Object.setPrototypeOf(specialized, subject)
+      should(specialized[propName]).equal(propValue) // check inheritance — test code validity
+
+      it(`reports false if the property is not an own property, and enumerable === ${enumerable} configurable === ${configurable} writable === ${writable}`, function () {
+        const specializedResult = isFrozenOwnProperty(specialized, propName)
+        should(specializedResult).not.be.ok()
+      })
+    })
+
+    const notObjects = [0, false, '', 'lala']
+    notObjects.forEach(notAnObject => {
+      // cannot set a property on primitives
+      it(`reports false if the first parameter is a primitive (${typeof notAnObject})`, function () {
+        const result = isFrozenOwnProperty(notAnObject, propName)
+        should(result).not.be.ok()
+      })
+    })
+
+    const fCandidates = [undefined, function (): void {}]
+    x(truths, truths, fCandidates, fCandidates).forEach(([configurable, enumerable, get, set]) => {
+      const subject = {}
+      Object.defineProperty(subject, propName, {
+        configurable,
+        enumerable,
+        ...(get && { get }),
+        ...(set && { set })
+      })
+      if (
+        !configurable &&
+        enumerable &&
+        ((typeof get === 'function' && set === undefined) ||
+          (get === undefined && set === undefined)) /* writable is false by default, so it is frozen */ &&
+        Object.prototype.hasOwnProperty.call(subject, propName)
+      ) {
+        it('reports true if the property is an own property, and it is enumerable, and not configurable, has a getter, but not a setter', function () {
+          const result = isFrozenOwnProperty(subject, propName)
+          should(result).be.ok()
+        })
+      } else {
+        it(`reports false if the property is an own property, enumerable === ${enumerable} configurable === ${configurable} get === ${get} set === ${set}`, function () {
+          const result = isFrozenOwnProperty(subject, propName)
+          should(result).not.be.ok()
+        })
+      }
     })
   })
 })
