cdbbc9b3b9826552f9cff602bacf7f6e9799142c
Jan Dockx <jandockx@gmail.com>
2025-01-20 13:20:25 +0100

    try to make type of bless correct and more simple, but …
    now we get  error TS2589: Type instantiation is excessively deep and possibly infinite. in contractFunctionBind

--- src/BaseFunctionContract.ts
+++ src/BaseFunctionContract.ts
@@ -132,11 +132,12 @@ export function isAGeneralContractFunction(
  * @returns {@link contractFunctionToBe}, as a {@link isAGeneralContractFunction}
  */
 export function bless<
-  ContractSignature extends UnknownFunction,
-  BFC extends BaseFunctionContract<ContractSignature, GeneralLocation>,
-  ImplementationSignature extends ContractSignature
+  BFC extends BaseFunctionContract<UnknownFunction, GeneralLocation>,
+  ImplementationSignature extends UnknownFunction
 >(
-  contractFunctionToBe: ContractSignature, // MUDO will become a Proxy, should be at least ImplementationSignature
+  contractFunctionToBe: BFC extends BaseFunctionContract<infer ContractSignature, GeneralLocation>
+    ? ContractSignature
+    : never, // MUDO will become a Proxy, should be at least ImplementationSignature
   contract: BFC,
   implFunction: ImplementationSignature,
   implementationLocation: string
